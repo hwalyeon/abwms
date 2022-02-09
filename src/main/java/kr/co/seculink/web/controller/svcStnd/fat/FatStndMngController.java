e958ca7649717c363661b8b46b0b41ecda385993
hyesoo <010-5225@naver.com>
2022-02-09 16:21:48 +0900

    비만기준 내용생성 [기준관리>비만>비만기준]

---
+++ src/main/java/kr/co/seculink/web/controller/svcStnd/fat/FatStndMngController.java
@@ -0,0 +1,126 @@
+package kr.co.seculink.web.controller.svcStnd.fat;
+
+import kr.co.seculink.domain.RtnMsg;
+import kr.co.seculink.exception.BizException;
+import kr.co.seculink.util.GEUtil;
+import kr.co.seculink.web.excel.ExcelConstant;
+import kr.co.seculink.web.service.svcStnd.fat.FatStndMngService;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.*;
+
+@Slf4j
+@Controller
+public class FatStndMngController
+{
+	@Resource(name="sqlSessionTemplate")
+	private SqlSessionTemplate dao;
+
+	@Autowired
+	private FatStndMngService fatStndMngService;
+
+	//비만_기준_리스트_조회
+	@ResponseBody
+	@RequestMapping("/svcStnd/fat/fatStndMng/searchFatStndList.ab")
+	public RtnMsg searchFatStndList(@RequestBody(required=false) Map<String, String> params) throws BizException
+	{
+		RtnMsg vo = new RtnMsg();
+		Map<String, Object> rtnMap = new HashMap<String, Object>();
+
+		List<Map<String, String>> result = fatStndMngService.searchFatStndList(params);
+
+		if ( !GEUtil.isEmpty(params.get("paging")) ) {
+			params.put("paging", "N");
+			vo.setTotalCount(((List)fatStndMngService.searchFatStndList(params)).size());
+		}
+		rtnMap.put("result", result);
+		vo.setRtnData(rtnMap, params);
+
+		return vo;
+	}
+	//비만_기준_버전_리스트_조회
+	@ResponseBody
+	@RequestMapping("/svcStnd/fat/fatStndMng/searchFatStndList.ab")
+	public RtnMsg fatStndVerList(@RequestBody(required=false) Map<String, String> params) throws BizException
+	{
+		RtnMsg vo = new RtnMsg();
+		Map<String, Object> rtnMap = new HashMap<String, Object>();
+
+		List<Map<String, String>> result = fatStndMngService.fatStndVerList(params);
+		rtnMap.put("result", result);
+
+		vo.setRtnData(rtnMap, params);
+
+		return vo;
+	}
+
+	//나이_년수 목록 조회
+	@ResponseBody
+	@RequestMapping("/svcStnd/fat/fatStndMng/ageYcntList.ab")
+	public RtnMsg ageYcntList(@RequestBody(required=false) Map<String, String> params) throws BizException
+	{
+		RtnMsg vo = new RtnMsg();
+		Map<String, Object> rtnMap = new HashMap<String, Object>();
+
+		List<Map<String, String>> result = fatStndMngService.ageYcntList(params);
+		rtnMap.put("result", result);
+
+		vo.setRtnData(rtnMap, params);
+
+		return vo;
+	}
+
+	//비만_기준_리스트 엑셀다운로드
+	@ResponseBody
+	@RequestMapping("/svcStnd/fat/fatStndMng/searchFatStndList/excel.ab")
+	public ModelAndView downloadExcel(@RequestBody(required=false) Map<String, String> params) throws BizException
+	{
+		params.put("paging", "N");
+
+		List<Map<String, String>> result = dao.selectList("svcStnd.fat.fatStndMng.searchFatStndList", params);
+		return new ModelAndView("excelXlsView", getExcelMap(result));
+	}
+
+	private Map<String, Object> getExcelMap(List<Map<String, String>> list)
+	{
+		String [] arrHeader = {"비만기준버전","비만기준번호","성별","나이(년)","나이(개월)","백분위5 비만지수","백분위50 비만지수","백분위95 비만지수","등록일자","등록시각","등록사용자ID","수정등록일자","수정시각","수정사용자ID"};
+		List<String> headerList = Arrays.asList(arrHeader);
+
+		List<List<String>> dataList = new ArrayList<List<String>>();
+		List<String> data;
+
+		for ( Map<String, String> info : list )
+		{
+			data = new ArrayList<String>();
+			data.add(info.get("fatStndVer"));
+			data.add(String.valueOf(info.get("fatStndNo")));
+			data.add(info.get("fnGetcdnm"));
+			data.add(String.valueOf(info.get("ageYcnt")));
+			data.add(String.valueOf(info.get("ageMcnt")));
+			data.add(String.valueOf(info.get("p3Gidx")));
+			data.add(String.valueOf(info.get("p50Gidx")));
+			data.add(String.valueOf(info.get("p97Gidx")));
+			data.add(info.get("regDt"));
+			data.add(info.get("regTm"));
+			data.add(info.get("regUserId"));
+			data.add(info.get("uptDt"));
+			data.add(info.get("uptTm"));
+			data.add(info.get("uptUserId"));
+			dataList.add(data);
+		}
+
+		Map<String, Object> map = new HashMap<>();
+		map.put(ExcelConstant.FILE_NAME, "excel");
+		map.put(ExcelConstant.HEAD, headerList);
+		map.put(ExcelConstant.BODY, dataList);
+		return map;
+	}
+}
