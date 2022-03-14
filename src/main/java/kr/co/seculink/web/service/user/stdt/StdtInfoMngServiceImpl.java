841b96cbf049a569297cffe0a748bcba064b8e02
화련 <woo0102923@naver.com>
2022-03-14 17:49:52 +0900

    학생정보 Grid에 BandId, 전화번호 등 클릭 시 화면 개발중

--- src/main/java/kr/co/seculink/web/service/user/stdt/StdtInfoMngServiceImpl.java
+++ src/main/java/kr/co/seculink/web/service/user/stdt/StdtInfoMngServiceImpl.java
@@ -1,6 +1,8 @@
 package kr.co.seculink.web.service.user.stdt;
 
+import kr.co.seculink.domain.RtnMsg;
 import kr.co.seculink.exception.BizException;
+import kr.co.seculink.util.GEUtil;
 import kr.co.seculink.web.service.cmon.FileService;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +24,7 @@ public class StdtInfoMngServiceImpl implements StdtInfoMngService
 	@Resource(name="sqlSessionTemplate")
 	private SqlSessionTemplate dao;
 
+	//학생정보 리스트 조회
 	public List<Map<String, String>> searchStdtInfoList(Map<String, String> params) throws BizException
 	{
 		List<Map<String, String>> result = dao.selectList("user.stdt.stdtInfoMng.searchStdtInfoList", params);
@@ -28,5 +32,92 @@ public class StdtInfoMngServiceImpl implements StdtInfoMngService
 		return result;
 	}
 
+	//밴드ID 중복 조회
+	public Map<String, String> searchDupBandId(Map<String, String> params) throws BizException
+	{
+		Map<String, String> result = dao.selectOne("user.stdt.stdtInfoMng.searchDupBandId", params);
+
+		Map<String, String> rtnMap = new HashMap<>();
+		if (result == null || GEUtil.isEmpty(result.get("bandId")) ) {
+			rtnMap.put("existsYn", "N");
+		} else {
+			rtnMap.put("existsYn", "Y");
+		}
+
+		return rtnMap;
+	}
+
+	//밴드ID 채번
+	public Map<String, Object> numberingBandId(Map<String, String> params) throws BizException
+	{
+		String bandYtypCd = (params.get("bandYtypCd")).substring(0, 3);
+		params.put("bandYtypCd", bandYtypCd);
+
+
+		Map<String, String> result = dao.selectOne("user.stdt.stdtInfoMng.numberingBandId", params);
+
+		Map<String, Object> rtnMap = new HashMap<>();
+
+		rtnMap.put("result", result);
+
+		return rtnMap;
+	}
+
+	//밴드_정보 저장
+	public void saveBandOpenInfoDetl(Map<String, Object> params) throws BizException {
+
+		RtnMsg vo = new RtnMsg();
+		Map<String, Object> rtnMap = new HashMap<String, Object>();
+
+		int saveCnt = 0;
+
+		if ("C".equals(params.get("crud"))) {
+			saveCnt += dao.insert("devc.band.bandOpenInfoMng.insertTsBandInfoList", params);
+		} else if ("U".equals(params.get("crud"))) {
+			saveCnt += dao.update("devc.band.bandOpenInfoMng.updateTsBandInfoList", params);
+		} else if ("D".equals(params.get("crud"))) {
+			saveCnt += dao.delete("devc.band.bandOpenInfoMng.deleteTsBandInfoList", params);
+		}
+		if (saveCnt == 0) {
+			throw new BizException("ECOM999", new String[]{"밴드정보 저장이 실패하였습니다."});
+		}
+
+	}
+
+	//밴드_정보_상세보기
+	public Map<String, Object> searchBandOpenInfo(Map<String, String> params) throws BizException
+	{
+
+		Map<String, String> result = dao.selectOne("devc.band.bandOpenInfoMng.selectBandOpenInfo", params);
+
+		Map<String, Object> rtnMap = new HashMap<>();
+
+		rtnMap.put("result", result);
+
+		return rtnMap;
+	}
+
+	//행추가_행삭제 저장
+	public void saveBandOpenDetlGuarTelNo(Map<String, Object> params) throws BizException
+	{
+		int saveCnt = 0;
+
+		List<Map<String, String>> gridDate = (List<Map<String, String>>) params.get("gridList");
+
+		for(Map<String,String> info:gridDate){
+			log.debug("crud         : " +  info.get("crud"));
+
+			if( "C".equals(info.get("crud"))){
+				saveCnt += dao.insert("svcStnd.nutr.bandOpenInfoMng.insertTmBandSpecList", info);
+			}else if( "U".equals(info.get("crud"))){
+				saveCnt += dao.update("svcStnd.nutr.bandOpenInfoMng.updateTmBandSpecList", info);
+			}else if( "D".equals(info.get("crud"))){
+				saveCnt += dao.delete("svcStnd.nutr.bandOpenInfoMng.deleteTmBandSpecList", info);
+			}
+		}
+		if(saveCnt == 0){
+			throw new BizException("ECOM999", new String[]{"밴드정보정보 저장이 실패하였습니다"});
+		}
+	}
 }
 
--- src/main/resources/static/js/user/stdt/stdtInfoMng.js
+++ src/main/resources/static/js/user/stdt/stdtInfoMng.js
@@ -91,23 +91,27 @@ let stdtInfoMng = new Vue({
                 {name: "prntNm2"           , index: "prntNm2"           , label: "학부모명2" 	    , width: 100 , align: "center" , fixed: true},
                 {name: "stdtNo"            , index: "stdtNo"            , label: "학생번호"		 	, width: 80 , align: "center" , fixed: true},
                 {name: "stdtNm"            , index: "stdtNm"            , label: "학생명"		 	, width: 80 , align: "center" , fixed: true},
-                {name: "telNo"             , index: "telNo"             , label: "전화번호"		 	, width: 100 , align: "center"
-                    ,formatter:function(cellValue, options, rowObject)   { return phoneFormatter(cellValue);}, fixed: true},
+                {name: "telNo"            , index: "telNo"              , label: "전화번호"		    , width: 100 , align: "center" , fixed: true  , formatter: function(cellValue, options, rowObject) {
+                        let telNoTemp = phoneFormatter(cellValue);
+                        return `<a data-toggle="modal" class="links" data-target="#bandOpenInfoDetlPopup" data-band data-placement="bottom" title="${telNoTemp}" data-band-id="${rowObject.bandId}">${telNoTemp}</a>`;}},
                 {name: "plcClssCd"         , index: "plcClssCd"         , label: "현재위치분류"		, width: 80  , align: "center" , fixed: true},
                 {name: "currLoc"           , index: "currLoc"           , label: "현재위치(주소)"	    , width: 250 , align: "center" , fixed: true},
                 {name: "dgemStatCd"        , index: "dgemStatCd"        , label: "위험감정상태"	 	, width: 80  , align: "center" , fixed: true},
                 {name: "strsIdx"           , index: "strsIdx"           , label: "스트레스상태"   	, width: 80  , align: "center" , fixed: true},
                 {name: "growIdx"           , index: "growIdx"           , label: "성장상태"		 	, width: 80  , align: "center" , fixed: true},
                 {name: "fatIdx"            , index: "fatIdx"            , label: "비만상태"		 	, width: 80  , align: "center" , fixed: true},
-                {name: "bandId"            , index: "bandId"            , label: "밴드ID"		 	, width: 80  , align: "center" , fixed: true},
-                {name: "blthId"            , index: "blthId"            , label: "블루투스ID"	 	, width: 100 , align: "center" , fixed: true},
+                {name: "bandId"           , index: "bandId"             , label: "밴드ID"		    , width: 80  , align: "center" , fixed: true  , formatter: function(cellValue, options, rowObject) {
+                        return `<a data-toggle="modal" class="links" data-target="#bandOpenInfoDetlPopup" data-band data-placement="bottom" title="${cellValue}" data-band-id="${rowObject.bandId}">${cellValue}</a>`;}},
+                {name: "blthId"           , index: "blthId"             , label: "블루투스ID"         , width: 100 , align: "center" , fixed: true  , formatter: function(cellValue, options, rowObject) {
+                        return `<a data-toggle="modal" class="links" data-target="#bandOpenInfoDetlPopup" data-band data-placement="bottom" title="${cellValue}" data-band-id="${rowObject.bandId}">${cellValue}</a>`;}},
                 {name: "sexCd"             , index: "sexCd"             , label: "성별"  		 	, width: 80  , align: "center" , fixed: true},
                 {name: "bithDt"            , index: "bithDt"            , label: "생년월일"		 	, width: 80  , align: "center"
                     , formatter: function(cellValue, options, rowObject) { return formatDate(cellValue);}        , fixed: true},
                 {name: "locNo"             , index: "locNo"             , label: "소속학교번호"	 	, width: 100 , align: "center" , fixed: true},
                 {name: "locNm"             , index: "locNm"             , label: "소속학교명"		 	, width: 100 , align: "center" , fixed: true},
                 {name: "age"               , index: "age"               , label: "(만)나이"  		, width: 80  , align: "center" , fixed: true},
-                {name: "bandStatCd"        , index: "bandStatCd"        , label: "밴드상태"		 	, width: 80  , align: "center" , fixed: true},
+                {name: "bandStatCd"        , index: "bandStatCd"        , label: "밴드<br/>개통상태"  , width: 60  , align: "center" , fixed: true  , formatter: function(cellValue, options, rowObject) {
+                        return `<a data-toggle="modal" class="links" data-target="#bandOpenInfoDetlPopup" data-band data-placement="bottom" title="${cellValue}" data-band-id="${rowObject.bandId}">${cellValue}</a>`;}},
                 {name: "mentStrsStatCd"    , index: "mentStrsStatCd"    , label: "신체적스트레스상태" 	, width: 110 , align: "center" , fixed: true},
                 {name: "physStrsStatCd"    , index: "physStrsStatCd"    , label: "정신적스트레스상태" 	, width: 110 , align: "center" , fixed: true},
                 {name: "useTotal"          , index: "useTotal"          , label: "캐시비 적립 총액"	, width: 125 , align: "center"
@@ -155,9 +159,11 @@ let stdtInfoMng = new Vue({
                 gridComplete: function() {
                     let grid = this;
 
-                    $(grid).tableRowSpan(["prntNm","prntNm2","stdtNo","stdtNm","telNo","plcClssCd","currLoc","dgemStatCd","strsIdx","growIdx","fatIdx","bandId","blthId","sexCd","bithDt","locNo","locNm","age","bandStatCd","mentStrsStatCd","physStrsStatCd","useTotal","saveTotal","cbeeBal","prntInfoDetlPopup"], "stdtNo");
+                    $(grid).tableRowSpan(["prntNm","prntNm2","stdtNo","locNo","stdtNm","telNo","plcClssCd","currLoc","dgemStatCd","strsIdx","growIdx","fatIdx","bandId","blthId","sexCd","bithDt","locNm","age","bandStatCd","mentStrsStatCd","physStrsStatCd","useTotal","saveTotal","cbeeBal","prntInfoDetlPopup"], "stdtNo");
 
-
+                    $("#grid_list").find('A.links[data-band]').on('click', function(e) {
+                        stdtInfoMng.regBandOpenInfoDetlPopup($(e.target).data('band-id'))
+                    });
                 }
             }));
 
@@ -192,6 +198,9 @@ let stdtInfoMng = new Vue({
         },
         regGrowJudgStndPop: function(growJudgCd) {
             growJudgStndDetl.initPage(growJudgCd);
+        },
+        regBandOpenInfoDetlPopup: function(bandId) {
+            bandOpenInfoDetl.initPage(bandId, function(){  bandOpenInfoMng.searchBandOpenInfoList(true) });
         },
         downloadExcel : function()
         {
