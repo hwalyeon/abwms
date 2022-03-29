690ed4bd5a228ae12d3a67631b39f0e2d21cf507
hyesoo <010-5225@naver.com>
2022-03-29 19:10:45 +0900

    [성장/비만지수 이력] 수정

--- src/main/resources/static/js/oper/hc/gfixHist.js
+++ src/main/resources/static/js/oper/hc/gfixHist.js
@@ -1,12 +1,12 @@
 let gfixHist = new Vue({
-    el: "#gfixHist", //스트레스_지수_이력
+    el: "#gfixHist", //성장/비만_지수_이력
     data:
     {
     	params:
         {
             userId         : '' ,
-            strsJudgDttmFr : '' , //스트레스_판정_일시(FROM)
-            strsJudgDttmTo : '' , //스트레스_판정_일시(To)
+            gfixDtFr       : '' , //성장비만지수_일자(FROM)
+            gfixDtTo       : '' , //성장비만지수_일자(To)
             bDPer          : 'THIS_MONTH' , //기준_일자_기간
             stdtNo         : '' , //학생_번호
             stdtNm         : '' , //학생_명
@@ -15,10 +15,9 @@ let gfixHist = new Vue({
             sexCd          : '' , //성별_코드
             guarNo         : '' , //보호자_번호
             guarNm         : '' , //보호자_명
-            strsStatCd     : '' , //스트레스_상태_코드
-            mindStrsStatCd : '' , //정신적_스트레스_상태_코드
-            physStrsStatCd : '' , //신체적_스트레스_상태_코드
-            strsCopeStatCd : '' , //스트레스_대처_상태_코드
+            growJudgCd     : '' , //성장_판정_코드
+            fatJudgCd      : '' , //비만_판정_코드
+            fatpJudgCd     : '' , //비만_예측_판정_코드
             locNm          : '' , //학교(학원)명
     		paging         : 'Y',
     		totalCount     : 0  ,
@@ -55,7 +54,7 @@ let gfixHist = new Vue({
             $this.code.bDPerList = CodeUtil.getPeriodDateList();
             const terms = getPeriodDate($this.params.bDPer);
             this.params.strsJudgDttmFr = terms.strDt;
-            this.params.strsJudgDttmTo = terms.endDt;
+            this.params.gfixDtTo = terms.endDt;
         },
         initCodeList : function()
         {
@@ -71,21 +70,28 @@ let gfixHist = new Vue({
             let $this = this;
         	let colModels =
             [
-                {name: "strsJudgDttm"     , index: "strsJudgDttm"     , label: "발생 일시" 	            ,  width: 50 , align: "center" },
-                {name: "locNm"            , index: "locNm"            , label: "학교 명" 	            ,  width: 50 , align: "center" },
-                {name: "stdtNo"           , index: "stdtNo"           , label: "학생 번호" 	            ,  width: 50 , align: "center" },
-                {name: "stdtNm"           , index: "stdtNm"           , label: "학생 명" 	            ,  width: 50 , align: "center" },
-                {name: "sexCdNm"          , index: "sexCdNm"          , label: "성별"	                ,  width: 50 , align: "center" },
-                {name: "ageYy"            , index: "ageYy"            , label: "나이(년)"                , width: 50 , align: "center" },
-                {name: "ageMm"            , index: "ageMm"            , label: "나이(개월)"              , width: 50 , align: "center" },
-                {name: "strsStatCdNm"     , index: "strsStatCdNm"     , label: "스트레스 상태"           , width: 50 , align: "center" },
-                {name: "mindStrsStatCdNm" , index: "mindStrsStatCdNm" , label: "정신적 상태"             , width: 50 , align: "center" },
-                {name: "physStrsStatCdNm" , index: "physStrsStatCdNm" , label: "신체적 상태"             , width: 50 , align: "center" },
-                {name: "strsCopeStatCd"   , index: "strsCopeStatCd"   , label: "대처 능력"               , width: 50 , align: "center" },
-                {name: "telNo"            , index: "telNo"            , label: "학생(밴드)<br/>전화번호" , width: 50 , align: "center"  ,formatter:function(cellValue, options, rowObject){ return phoneFormatter(cellValue);}},
-                {name: "guarNo"           , index: "guarNo"           , label: "보호자 번호"             , width: 50 , align: "center" },
-                {name: "guarNm"           , index: "guarNm"           , label: "보호자 명"               , width: 50 , align: "center" },
-                {name: "guarTelNo" 	      , index: "guarTelNo" 	      , label: "보호자<br/>전화번호"     , width: 50 , align: "center"  ,formatter:function(cellValue, options, rowObject){ return phoneFormatter(cellValue);}}
+                {name: "gfixDt"       , index: "gfixDt"       , label: "기준 일자" 	            ,  width: 50 , align: "center" },
+                {name: "locNm"        , index: "locNm"        , label: "학교 명" 	            ,  width: 50 , align: "center" },
+                {name: "stdtNo"       , index: "stdtNo"       , label: "학생 번호" 	            ,  width: 50 , align: "center" },
+                {name: "stdtNm"       , index: "stdtNm"       , label: "학생 명" 	            ,  width: 50 , align: "center" },
+                {name: "sexCdNm"      , index: "sexCdNm"      , label: "성별"	                ,  width: 50 , align: "center" },
+                {name: "ageYcnt"      , index: "ageYcnt"      , label: "나이(년)"                , width: 50 , align: "center" },
+                {name: "ageMcnt"      , index: "ageMcnt"      , label: "나이(개월)"              , width: 50 , align: "center" },
+                {name: "hghtVal"      , index: "hghtVal"      , label: "키"                      , width: 50 , align: "center" },
+                {name: "wghtVal"      , index: "wghtVal"      , label: "몸무게"                  , width: 50 , align: "center" },
+                {name: "calEatQty"    , index: "calEatQty"    , label: "칼로리 섭취/일"          , width: 50 , align: "center" },
+                {name: "calCsumQty"   , index: "calCsumQty"   , label: "칼로리 소모/일"          , width: 50 , align: "center" },
+                {name: "growIdx"      , index: "growIdx"      , label: "성장지수"                , width: 50 , align: "center" },
+                {name: "growJudgCdNm" , index: "growJudgCdNm" , label: "성장 판정"               , width: 50 , align: "center" },
+                {name: "fatIdx"       , index: "fatIdx"       , label: "비만 지수"               , width: 50 , align: "center" },
+                {name: "fatJudgCdNm"  , index: "fatJudgCdNm"  , label: "비만 판정"               , width: 50 , align: "center" },
+                {name: "fatpIdx"      , index: "fatpIdx"      , label: "비만 예측지수"           , width: 50 , align: "center" },
+                {name: "fatpJudgCdNm" , index: "fatpJudgCdNm" , label: "비만 예측"               , width: 50 , align: "center" },
+                {name: "palVal"       , index: "palVal"       , label: "신체활동 수준"           , width: 50 , align: "center" },
+                {name: "telNo"        , index: "telNo"        , label: "학생(밴드)<br/>전화번호" , width: 50 , align: "center"  ,formatter:function(cellValue, options, rowObject){ return phoneFormatter(cellValue);}},
+                {name: "guarNo"       , index: "guarNo"       , label: "보호자 번호"             , width: 50 , align: "center" },
+                {name: "guarNm"       , index: "guarNm"       , label: "보호자 명"               , width: 50 , align: "center" },
+                {name: "guarTelNo" 	  , index: "guarTelNo" 	  , label: "보호자<br/>전화번호"     , width: 50 , align: "center"  ,formatter:function(cellValue, options, rowObject){ return phoneFormatter(cellValue);}}
             ];
             $("#gfixHist_list").jqGrid("GridUnload");
            	$("#gfixHist_list").jqGrid($.extend(true, {}, commonGridOptions(),
@@ -109,12 +115,13 @@ let gfixHist = new Vue({
                 gridComplete: function () {
                     let grid = this;
 
-                    $(grid).tableRowSpan(["strsJudgDttm","locNm","stdtNo","stdtNm","sexCdNm", "ageYy","ageMm", "strsStatCdNm","mindStrsStatCdNm","physStrsStatCdNm","strsCopeStatCd","telNo"], "stdtNo");
+                    $(grid).tableRowSpan(["locNm","stdtNo","stdtNm","sexCdNm", "ageYcnt","ageMcnt", "hghtVal","wghtVal"], "stdtNo");
+                    $(grid).tableRowSpan(["gfixDt","calEatQty","calCsumQty","growIdx","growJudgCdNm", "fatIdx","fatJudgCdNm", "fatpIdx","fatpJudgCdNm","palVal"], "gfixDt");
                 }
             }));
             resizeJqGridWidth("gfixHist_list", "gfixHist_list_wrapper");
         },
-        //스트레스_지수_이력 리스트 조회
+        //성장/비만_지수_이력 리스트 조회
         searchGfixHistList: function(isSearch)
         {
 			let $this     = this;
@@ -143,7 +150,7 @@ let gfixHist = new Vue({
                 }
             }).trigger("reloadGrid");
 		},
-        //스트레스_지수_이력 리스트 엑셀 다운로드
+        //성장/비만_지수_이력 리스트 엑셀 다운로드
 		downloadExcel : function()
         {
 			let $this = this;
@@ -170,7 +177,7 @@ let gfixHist = new Vue({
             let $this = this;
             const terms = getPeriodDate($this.params.bDPer);
             this.params.strsJudgDttmFr = terms.strDt;
-            this.params.strsJudgDttmTo = terms.endDt;
+            this.params.gfixDtTo = terms.endDt;
         },
         //기준_일자_선택
         bDSelect: function()
@@ -192,12 +199,12 @@ let gfixHist = new Vue({
                 Swal.alert(['정확한 나이 범위를 입력하세요.', 'info']);
                 return false;
             }
-            if( ((WebUtil.isNotNull($this.params.strsJudgDttmFr)) && (WebUtil.isNull($this.params.strsJudgDttmTo))) || ((WebUtil.isNotNull($this.params.strsJudgDttmTo)) && (WebUtil.isNull($this.params.strsJudgDttmFr))) )
+            if( ((WebUtil.isNotNull($this.params.strsJudgDttmFr)) && (WebUtil.isNull($this.params.gfixDtTo))) || ((WebUtil.isNotNull($this.params.gfixDtTo)) && (WebUtil.isNull($this.params.strsJudgDttmFr))) )
             {
                 Swal.alert(['나머지 기준 일자를 선택하세요.', 'info']);
                 return false;
             }
-            if( ((WebUtil.isNotNull($this.params.strsJudgDttmFr) && WebUtil.isNotNull($this.params.strsJudgDttmTo)) && $this.params.strsJudgDttmFr > $this.params.strsJudgDttmTo) )
+            if( ((WebUtil.isNotNull($this.params.strsJudgDttmFr) && WebUtil.isNotNull($this.params.gfixDtTo)) && $this.params.strsJudgDttmFr > $this.params.gfixDtTo) )
             {
                 Swal.alert(['정확한 기준 일자를 선택하세요.', 'info']);
                 return false;
@@ -211,7 +218,7 @@ let gfixHist = new Vue({
             {
                 userId         : '' ,
                 strsJudgDttmFr : '' , //스트레스_판정_일시(FROM)
-                strsJudgDttmTo : '' , //스트레스_판정_일시(To)
+                gfixDtTo : '' , //스트레스_판정_일시(To)
                 bDPer          : 'THIS_MONTH' , //기준_일자_기간
                 stdtNo         : '' , //학생_번호
                 stdtNm         : '' , //학생_명
