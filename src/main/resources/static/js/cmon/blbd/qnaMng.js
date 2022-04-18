5a586961ed972be72140e2acbd99c5ede8d53fa7
hyesoo <010-5225@naver.com>
2022-04-18 17:02:13 +0900

    [질의응답관리] 화면생성

---
+++ src/main/resources/static/js/cmon/blbd/qnaMng.js
@@ -0,0 +1,279 @@
+let qnaMng = new Vue({
+    el: "#qnaMng", //위험감정_상태_기준
+    data:
+    {
+    	params:
+        {
+            userId       : ''  ,
+            qnaNo        : ''  , //질의응답_번호
+            qustGuarNo   : ''  , //지문_보호자_번호
+            qustDtFr     : ''  , //질문_일자(FROM)
+            qustDtTo     : ''  , //질문_일자(To)
+            bDPer        : ''  , //질문_일자_기간
+            qustCntn     : ''  , //질문_내용
+            ansDtFr      : ''  , //답변_일자(FROM)
+            ansDtTo      : ''  , //답변_일자(To)
+            bDPerAns     : ''  , //답변_일자_기간
+            ansCntn      : ''  , //답변_내용
+    		paging       : 'Y' ,
+    		totalCount   : 0   ,
+            rowCount     : 30  ,
+            currentPage  : 1   ,
+            currentIndex : 0
+        },
+        code:
+        {
+            bDPerList      : [] , //질문_일자_기간_리스트
+            bDPerAnsList   : [] , //답변_일자_기간_리스트
+        },
+	},
+    methods:
+    {
+        initialize: function()
+        {
+        	let $this = this;
+
+            $this.initValue();
+        	$this.initCodeList();
+        	$this.initGrid();
+            $this.searchDgemList(true);
+            $this.setDatepicker();
+        },
+        initValue: function()
+        {
+            let $this = this;
+            $this.userId = SessionUtil.getUserId();
+            $this.code.bDPerList    = CodeUtil.getPeriodDateList(); //질문_일자_기간_리스트
+            $this.code.bDPerAnsList = CodeUtil.getPeriodDateList(); //답변_일자_기간_리스트
+        },
+        initCodeList : function()
+        {
+            let $this = this;
+
+        },
+        initGrid: function()
+        {
+        	 let $this = this;				                                                             
+             
+        	let colModels = [
+                {name: "crud"           , index: "crud"           , label: "crud"         , hidden: true },
+                {name: "actDivCdTemp"   , index: "actDivCdTemp"   , label: "활동 구분 코드" 	  , hidden: true },
+                {name: "hbitStatCdTemp" , index: "hbitStatCdTemp" , label: "심박 상태 코드" 	  , hidden: true }, 
+                {name: "plcClssCdTemp"  , index: "plcClssCdTemp"  , label: "장소 분류 코드" 	  , hidden: true },
+                {name: "tempStatCdTemp" , index: "tempStatCdTemp" , label: "체온 상태 코드" 	  , hidden: true },
+                {name: "dgemStatCdTemp" , index: "dgemStatCdTemp" , label: "위험감정 상태 코드" , hidden: true },
+                {name: "actDivCd"       , index: "actDivCd"       , label: "활동 구분 코드" 	  , hidden: true },
+                {name: "hbitStatCd"     , index: "hbitStatCd"     , label: "심박 상태 코드" 	  , hidden: true }, 
+                {name: "plcClssCd"      , index: "plcClssCd"      , label: "장소 분류 코드" 	  , hidden: true },
+                {name: "tempStatCd"     , index: "tempStatCd"     , label: "체온 상태 코드" 	  , hidden: true },
+                {name: "dgemStatCd"     , index: "dgemStatCd"     , label: "위험감정 상태 코드" , hidden: true },
+                {name: "actDivCdNm"     , index: "actDivCdNm"     , label: "활동 구분 명" 	  , width: 50 , align: "center" },
+                {name: "hbitStatCdNm"   , index: "hbitStatCdNm"   , label: "심박 상태 명" 	  , width: 50 , align: "center" },
+                {name: "plcClssCdNm"    , index: "plcClssCdNm"    , label: "장소 분류 명" 	  , width: 50 , align: "center" },
+                {name: "tempStatCdNm"   , index: "tempStatCdNm"   , label: "체온 상태 명" 	  , width: 50 , align: "center" },
+                {name: "dgemIdx" 	    , index: "dgemIdx" 	      , label: "위험감정 지수" 	  , width: 50 , align: "center" },
+                {name: "dgemStatCdNm"   , index: "dgemStatCdNm"   , label: "위험감정 상태 명"    , width: 50 , align: "center" }, 
+                {name: "dgemSmryCntn"   , index: "dgemSmryCntn"   , label: "위험감정 요약내용" , width: 80 , align: "center" 	}, 
+                {name: "regDt"          , index: "regDt"          , label: "등록일자"   	  , width: 50 , align: "center"  , formatter: function(cellValue, options, rowObject) { return formatDate(cellValue);}  },
+                {name: "regTm"          , index: "regTm"          , label: "등록시각"   	  , width: 50 , align: "center"  , formatter: function(cellValue, options, rowObject) { return formatTime(cellValue);}  },
+                {name: "regUserId"      , index: "regUserId"      , label: "등록사용자ID"	  , width: 50 , align: "center"  },
+                {name: "uptDt"          , index: "uptDt"          , label: "수정일자"    	  , width: 50 , align: "center"  , formatter: function(cellValue, options, rowObject) { return formatDate(cellValue);}  },
+                {name: "uptTm"          , index: "uptTm"          , label: "수정시각"    	  , width: 50 , align: "center"  , formatter: function(cellValue, options, rowObject) { return formatTime(cellValue);}  },
+                {name: "uptUserId"      , index: "uptUserId"      , label: "수정사용자ID"	  , width: 50 , align: "center"  },
+                {name: "dgemStndDetlPop" , index: "dgemStndDetlPop" , label: "상세정보보기", width: 50, align: "center",
+                    formatter: function(cellValue, options, rowObject) {
+                        return '<input type="button" class="btn btn-xs btn-outline btn-success" onclick="dgemStndMng.regDgemStndDetlPop(\'' + rowObject.actDivCd + '\',\'' + rowObject.hbitStatCd + '\',\'' + rowObject.plcClssCd + '\',\'' + rowObject.tempStatCd + '\',\'' + rowObject.dgemStatCd + '\')" value="상세보기" data-toggle="modal" data-target="#dgemStndDetlPopup" />';
+                    }
+                }
+                
+            ];
+  
+            $("#dgem_list").jqGrid("GridUnload");
+           	$("#dgem_list").jqGrid($.extend(true, {}, commonEditGridOptions(),
+            {
+            	datatype : "local",
+            	mtype    : 'post'  ,
+                url      : '/svcStnd/dgem/dgemStndMng/searchDgemList.ab',
+                pager    : '#dgem_pager_list',
+				height   : 405     ,
+                colModel : colModels,
+                onPaging : function(data)
+                {
+                    onPagingCommon(data, this, function(resultMap)
+                    {
+                        $this.params.currentPage  = resultMap.currentPage;
+                        $this.params.rowCount      = resultMap.rowCount;
+                        $this.params.currentIndex = resultMap.currentIndex;
+                        $this.searchDgemList(false);
+                    })
+                },
+                afterSaveCell : function (rowid , colId , val, e )
+                {
+
+                    if($("#dgem_list").getRowData(rowid).crud != "C" && $("#dgem_list").getRowData(rowid).crud != "D" )
+                    {
+                        $("#dgem_list").setRowData(rowid, {crud:"U"});
+                    }
+                }
+            }));
+            resizeJqGridWidth("dgem_list", "dgem_list_wrapper");
+        },
+        searchDgemList: function(isSearch)
+        {
+			let $this     = this;
+            let params = $.extend(true, {}, $this.params);
+			
+            if ( isSearch )
+            {
+                params.currentPage = 1;
+                params.currentIndex = 0;
+            }
+            
+			$("#dgem_list").setGridParam(
+			{
+                datatype: "json",
+                postData: JSON.stringify(params),
+                page: 1,
+                loadComplete: function (response)
+                {
+                    if ( response.rtnData.result == 0 )
+                    {
+                        Swal.alert(['조회할 내용이 없습니다.', "info"]);
+                    }
+                }
+            }).trigger("reloadGrid");
+		},
+		regDgemStndDetlPop: function(actDivCd, hbitStatCd, plcClssCd, tempStatCd, dgemStatCd) {
+			dgemStndDetl.initPage(actDivCd, hbitStatCd, plcClssCd, tempStatCd, dgemStatCd);
+        },
+        //기준_일자 선택
+        setDatepicker : function() {
+            let $this = this;
+
+            if($this.params.bDPer!=='')
+              {$this.params.bDPer = '' ;}
+
+            if($this.params.bDPerAns!=='')
+              {$this.params.bDPerAns = '' ;}
+
+            $('#qustDtFrPicker').datepicker({
+                todayBtn: "linked",
+                keyboardNavigation: false,
+                forceParse: false,
+                calendarWeeks: true,
+                autoclose: true,
+                todayHighlight: true,
+            }).on("changeDate", function() {
+                $this.params.qustDtFr = $('#qustDtFr').val();
+            });
+
+            $('#qustDtToPicker').datepicker({
+                todayBtn: "linked",
+                keyboardNavigation: false,
+                forceParse: false,
+                calendarWeeks: true,
+                autoclose: true,
+                todayHighlight: true,
+            }).on("changeDate", function() {
+                $this.params.qustDtTo = $('#qustDtTo').val();
+            });
+
+            $('#ansDtFrPicker').datepicker({
+                todayBtn: "linked",
+                keyboardNavigation: false,
+                forceParse: false,
+                calendarWeeks: true,
+                autoclose: true,
+                todayHighlight: true,
+            }).on("changeDate", function() {
+                $this.params.ansDtFr = $('#ansDtFr').val();
+            });
+
+            $('#ansDtToPicker').datepicker({
+                todayBtn: "linked",
+                keyboardNavigation: false,
+                forceParse: false,
+                calendarWeeks: true,
+                autoclose: true,
+                todayHighlight: true,
+            }).on("changeDate", function() {
+                $this.params.ansDtTo = $('#ansDtTo').val();
+            });
+        },
+        //질문_일자_기간_선택
+        bDPerSelect: function() {
+            let $this = this;
+            const terms = getPeriodDate($this.params.bDPer);
+            this.params.qustDtFr = terms.strDt;
+            this.params.qustDtTo = terms.endDt;
+        },
+
+        //답변_일자_기간_선택
+        bDPerAnsSelect: function() {
+            let $this = this;
+            const terms = getPeriodDate($this.params.bDPerAns);
+            this.params.ansDtFr = terms.strDt;
+            this.params.anstDtTo = terms.endDt;
+        },
+
+        downloadExcel : function()
+        {
+			let $this = this;
+			let params = $.extend(true, {}, $this.params);
+			
+			AjaxUtil.post(
+{
+				dataType : 'binary',
+                url            : "/svcStnd/dgem/dgemStndMng/searchDgemList/excel.ab",
+                param     : params,
+                success: function(response)
+                {
+                	saveFileLocal(response, 'dgemStndMng.xls');
+                },
+                error: function (response)
+                {
+                    Swal.alert([response, 'error']);
+                }
+            });
+		},
+
+		resetSearchParam: function()
+        {
+			let $this = this;
+			$this.params =
+            {
+                userId       : ''  ,
+                qnaNo        : ''  , //질의응답_번호
+                qustGuarNo   : ''  , //지문_보호자_번호
+                qustDtFr     : ''  , //질문_일자(FROM)
+                qustDtTo     : ''  , //질문_일자(To)
+                bDPer        : ''  , //질문_일자_기간
+                qustCntn     : ''  , //질문_내용
+                ansDtFr      : ''  , //답변_일자(FROM)
+                ansDtTo      : ''  , //답변_일자(To)
+                bDPerAns     : ''  , //답변_일자_기간
+                ansCntn      : ''  , //답변_내용
+                paging       : 'Y' ,
+                totalCount   : 0   ,
+                rowCount     : 30  ,
+                currentPage  : 1   ,
+                currentIndex : 0
+	    	}
+		}
+    },
+    computed:
+    {
+    },
+    watch:
+    {
+
+    },
+    mounted: function()
+    {
+        let self = this;
+        $(document).ready(function()
+        {
+            self.initialize();
+        });
+    }
+});
