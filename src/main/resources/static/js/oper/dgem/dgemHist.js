cd7d181453aa7708b54c157f2c91e69ec4e6d6d3
starthif <starthif@gmail.com>
2022-05-18 16:13:16 +0900

    위험판정이력/학생정보상세팝업 수정

--- src/main/resources/static/js/oper/dgem/dgemHist.js
+++ src/main/resources/static/js/oper/dgem/dgemHist.js
@@ -2,31 +2,32 @@ let dgemHist = new Vue({
     el: "#dgemHist",
     data: {
         params: {
-            entrDtFr       :'',  //일자_From
-            entrDtTo       :'',  //일자_To
-            stdtNo         :'',  //학생번호
-            dgemHistSeq    :'',  //위험감정 이력순번
-            dgemDt         :'',  //위험감정 일자
-            dgemTm         :'',  //위험감정 시각
-            dgemIdx        :'',  //위험감정 지수
-            dgemStatCd     :'',  //위험감정 상태코드
-            dgemStatCntn   :'',  //위험감정 요약내용
-            dgemSmryCntn   :'',  //위험감정 상태내용
-            locHistNo      :'',  //위치이력 번호
-            currLatVal     :'',  //현재위도 값
-            currLonVal     :'',  //현재경도 값
-            locMesuDttm    :'',  //위치측정일시
-            actDivCd       :'',  //활동구분코드
-            hbitStatCd     :'',  //심박상태코드
-            plcClssCd      :'',  //장소분류코드
-            tempStatCd     :'',  //체온상태코드
-            judgNo         :'',  //판정번호
-            rmrk           :'',  //비고
-            dgemAlamNo     :'',  //위험감정알림번호
-            locNm          :'',  //학교명
-            guarNo         :'',  //보호자번호
-            guarNm         :'',  //보호자명
-            guarTelNo      :'',  //보호자전화번호
+            entrDtFr       :'',  // 일자_From
+            entrDtTo       :'',  // 일자_To
+            stdtNo         :'',  // 학생번호
+            stdtNm         :'',  // 학생명
+            dgemHistSeq    :'',  // 위험감정 이력순번
+            dgemDt         :'',  // 위험감정 일자
+            dgemTm         :'',  // 위험감정 시각
+            dgemIdx        :'',  // 위험감정 지수
+            dgemStatCd     :'',  // 위험감정 상태코드
+            dgemStatCntn   :'',  // 위험감정 요약내용
+            dgemSmryCntn   :'',  // 위험감정 상태내용
+            locHistNo      :'',  // 위치이력 번호
+            currLatVal     :'',  // 현재위도 값
+            currLonVal     :'',  // 현재경도 값
+            locMesuDttm    :'',  // 위치측정일시
+            actDivCd       :'',  // 활동구분코드
+            hbitStatCd     :'',  // 심박상태코드
+            plcClssCd      :'',  // 장소분류코드
+            tempStatCd     :'',  // 체온상태코드
+            judgNo         :'',  // 판정번호
+            rmrk           :'',  // 비고
+            dgemAlamNo     :'',  // 위험감정알림번호
+            locNm          :'',  // 학교명
+            guarNo         :'',  // 보호자번호
+            guarNm         :'',  // 보호자명
+            guarTelNo      :'',  // 보호자전화번호
             mmDd           :'THIS_MONTH',
             paging         :'Y',
             totalCount     : 0,
@@ -86,35 +87,38 @@ let dgemHist = new Vue({
         {
             let $this = this;
             let colModels = [
-                {name: "crud"              , index: "crud"              , label: "crud"		 	    , hidden: true                                  },
-                {name: "guarNoTemp"        , index: "guarNoTemp"        , label: "보호자번호"			, width: 80     , align: "center" , hidden: true},
-                {name: "stdtHistSeq"       , index: "stdtHistSeq"       , label: "학생이력"			, width: 80     , align: "center" , hidden: true},
-                {name: "stdtGuarNm"        , index: "stdtGuarNm"        , label: "학생이력"			, width: 80     , align: "center" , hidden: true},
-                {name: "stdtNo"            , index: "stdtNo"            , label: "학생번호"	        , width: 80     , align: "center" , fixed:true , formatter: function(cellValue, options, rowObject){
-                        if(WebUtil.isNull(cellValue)) return '<input type="button" class="btn btn-xs btn-outline btn-success" onclick="dgemHist.regStdtInfoDetlPopup(\'' + rowObject.stdtNo + '\', \'' + rowObject.guarNo + '\')" value="신규" data-toggle="modal" data-target="#stdtRegDetlPopup" />';
-                        else return`<a data-toggle="modal" class="links" data-target="#stdtInfoDetlPopup" data-stdt data-placement="bottom" title="${cellValue}" data-guar-no="${rowObject.guarNo}" data-stdt-no="${rowObject.stdtNo}">${cellValue}</a>`;}},
-                {name: "dgemHistSeq"       , index: "dgemHistSeq"       , label: "위험감정 이력순번"	, width: 110    , align: "center" , fixed: true},
-                {name: "dgemDt"            , index: "dgemDt"            , label: "위험감정 일자"		, width: 80     , align: "center" , fixed: true},
-                {name: "dgemTm"            , index: "dgemTm"            , label: "위험감정 시각"		, width: 80     , align: "center" , fixed: true},
-                {name: "dgemIdx"           , index: "dgemIdx"           , label: "위험감정 지수"	    , width: 80     , align: "center" , fixed: true},
-                {name: "dgemStatCd"        , index: "dgemStatCd"        , label: "위험감정 상태코드"	, width: 110    , align: "center" , fixed: true},
-                {name: "dgemStatCntn"      , index: "dgemStatCntn"      , label: "위험감정 요약내용"   , width: 110    , align: "center" , fixed: true},
-                {name: "dgemSmryCntn"      , index: "dgemSmryCntn"      , label: "위험감정 상태내용"	, width: 110    , align: "center" , fixed: true},
-                {name: "locHistNo"         , index: "locHistNo"         , label: "위치이력 번호"		, width: 80     , align: "center" , fixed: true},
-                /*{name: "currLatVal"        , index: "currLatVal"        , label: "현재위도 값"		, width: 80     , align: "center" , fixed: true},
-                {name: "currLonVal"        , index: "currLonVal"        , label: "현재경도 값"  		, width: 80     , align: "center" , fixed: true},
-                {name: "locMesuDttm"       , index: "locMesuDttm"       , label: "위치측정일시"	 	, width: 100    , align: "center" , fixed: true},*/
-                {name: "actDivCd"          , index: "actDivCd"          , label: "활동구분코드"		, width: 100    , align: "center" , fixed: true},
-                {name: "hbitStatCd"        , index: "hbitStatCd"        , label: "심박상태코드"  		, width: 100    , align: "center" , fixed: true},
-                {name: "plcClssCd"         , index: "plcClssCd"         , label: "장소분류코드" 	    , width: 110    , align: "center" , fixed: true},
-                {name: "tempStatCd"        , index: "tempStatCd"        , label: "체온상태코드"    	, width: 110    , align: "center" , fixed: true},
-                {name: "judgNo"            , index: "judgNo"            , label: "판정번호" 	        , width: 110    , align: "center" , fixed: true},
-                {name: "rmrk"              , index: "rmrk"              , label: "비고" 	            , width: 110    , align: "center" , fixed: true},
-                {name: "dgemAlamNo"        , index: "dgemAlamNo"        , label: "위험감정알림번호" 	, width: 110    , align: "center" , fixed: true},
-                {name: "locNm"             , index: "locNm"             , label: "학교명"        	, width: 150    , align: "center" , fixed: true},
-                {name: "guarNo"            , index: "guarNo"            , label: "보호자번호" 	    , width: 110    , align: "center" , fixed: true},
-                {name: "guarNm"            , index: "guarNm"            , label: "보호자명" 	        , width: 110    , align: "center" , fixed: true},
-                {name: "guarTelNo"         , index: "guarTelNo"         , label: "보호자전화번호" 	    , width: 110    , align: "center" , fixed: true},
+                {name: "crud"              , index: "crud"              , label: "crud"                                                 , hidden: true},
+                {name: "guarNoTemp"        , index: "guarNoTemp"        , label: "보호자번호"         , width: 80     , align: "center" , hidden: true},
+                {name: "stdtHistSeq"       , index: "stdtHistSeq"       , label: "학생이력"           , width: 80     , align: "center" , hidden: true},
+                {name: "stdtGuarNm"        , index: "stdtGuarNm"        , label: "학생이력"           , width: 80     , align: "center" , hidden: true},
+                {name: "dgemDt"            , index: "dgemDt"            , label: "판정일자"           , width: 80     , align: "center" , fixed:  true , formatter: function(cellValue, options, rowObject) { if(WebUtil.isNull(cellValue)) return ''; else return formatDate(cellValue); }},
+                {name: "dgemTm"            , index: "dgemTm"            , label: "판정시각"           , width: 80     , align: "center" , fixed:  true , formatter: function(cellValue, options, rowObject) { if(WebUtil.isNull(cellValue)) return ''; else return formatTime(cellValue); }},
+                {name: "stdtNo"            , index: "stdtNo"            , label: "학생번호"           , width: 80     , align: "center" , fixed:  true , formatter: function(cellValue, options, rowObject){
+                    if(WebUtil.isNull(cellValue)) return '<input type="button" class="btn btn-xs btn-outline btn-success" onclick="dgemHist.regStdtInfoDetlPopup(\'' + rowObject.stdtNo + '\', \'' + rowObject.guarNo + '\')" value="신규" data-toggle="modal" data-target="#stdtRegDetlPopup" />';
+                    else return`<a data-toggle="modal" class="links" data-target="#stdtInfoDetlPopup" data-stdt data-placement="bottom" title="${cellValue}" data-guar-no="${rowObject.guarNo}" data-stdt-no="${rowObject.stdtNo}">${cellValue}</a>`;}},
+                {name: "stdtNm"            , index: "stdtNm"            , label: "학생명"             , width: 80     , align: "center" , fixed:  true , formatter: function(cellValue, options, rowObject){
+                    if(WebUtil.isNull(cellValue)) return '<input type="button" class="btn btn-xs btn-outline btn-success" onclick="dgemHist.regStdtInfoDetlPopup(\'' + rowObject.stdtNo + '\', \'' + rowObject.guarNo + '\')" value="신규" data-toggle="modal" data-target="#stdtRegDetlPopup" />';
+                    else return`<a data-toggle="modal" class="links" data-target="#stdtInfoDetlPopup" data-stdt data-placement="bottom" title="${cellValue}" data-guar-no="${rowObject.guarNo}" data-stdt-no="${rowObject.stdtNo}">${cellValue}</a>`;}},
+                {name: "dgemHistSeq"       , index: "dgemHistSeq"       , label: "위험감정 이력순번"  , width: 110    , align: "center" , fixed:  true},
+                {name: "dgemIdx"           , index: "dgemIdx"           , label: "위험감정 지수"      , width: 80     , align: "center" , fixed:  true},
+                {name: "dgemStatCd"        , index: "dgemStatCd"        , label: "위험감정 상태코드"  , width: 110    , align: "center" , fixed:  true},
+                {name: "dgemSmryCntn"      , index: "dgemSmryCntn"      , label: "위험감정 요약내용"  , width: 150    , align: "left"   , fixed:  true},
+                {name: "dgemStatCntn"      , index: "dgemStatCntn"      , label: "위험감정 상세내용"  , width: 200    , align: "left"   , fixed:  true},
+                {name: "locHistNo"         , index: "locHistNo"         , label: "위치이력 번호"      , width: 80     , align: "center" , fixed:  true},
+/*              {name: "currLatVal"        , index: "currLatVal"        , label: "현재위도 값"        , width: 80     , align: "center" , fixed:  true},
+                {name: "currLonVal"        , index: "currLonVal"        , label: "현재경도 값"        , width: 80     , align: "center" , fixed:  true},
+                {name: "locMesuDttm"       , index: "locMesuDttm"       , label: "위치측정일시"       , width: 100    , align: "center" , fixed:  true},*/
+                {name: "actDivCd"          , index: "actDivCd"          , label: "활동구분"           , width: 100    , align: "center" , fixed:  true},
+                {name: "hbitStatCd"        , index: "hbitStatCd"        , label: "심박상태"           , width: 100    , align: "center" , fixed:  true},
+                {name: "plcClssCd"         , index: "plcClssCd"         , label: "장소분류"           , width: 110    , align: "center" , fixed:  true},
+                {name: "tempStatCd"        , index: "tempStatCd"        , label: "온도상태"           , width: 110    , align: "center" , fixed:  true},
+                {name: "judgNo"            , index: "judgNo"            , label: "판정번호"           , width: 110    , align: "center" , fixed:  true},
+                {name: "rmrk"              , index: "rmrk"              , label: "비고"               , width: 110    , align: "center" , fixed:  true},
+                {name: "dgemAlamNo"        , index: "dgemAlamNo"        , label: "위험감정알림번호"   , width: 110    , align: "center" , fixed:  true},
+                {name: "locNm"             , index: "locNm"             , label: "학교명"             , width: 150    , align: "center" , fixed:  true},
+                {name: "guarNo"            , index: "guarNo"            , label: "보호자번호"         , width: 110    , align: "center" , fixed:  true},
+                {name: "guarNm"            , index: "guarNm"            , label: "보호자명"           , width: 110    , align: "center" , fixed:  true},
+                {name: "guarTelNo"         , index: "guarTelNo"         , label: "보호자전화번호"     , width: 110    , align: "center" , fixed:  true},
             ];
 
 
@@ -137,11 +141,11 @@ let dgemHist = new Vue({
                 },
                 gridComplete: function() {
                     let grid = this;
-
+/*
                     $(grid).tableRowSpan(["stdtNo","dgemHistSeq","dgemDt","dgemTm","dgemIdx","dgemStatCd","dgemStatCntn","dgemSmryCntn",
                         "locHistNo","currLatVal","currLonVal","locMesuDttm","actDivCd","hbitStatCd","plcClssCd","tempStatCd","judgNo","rmrk","dgemAlamNo","locNm"], "stdtHistSeq");
                     $(grid).tableRowSpan(["stdtNo","locNm","guarNo","guarNm","guarTelNo"], "stdtGuarNm");
-
+*/
                     //학생정보 상세 팝업
                     $("#dgem_list").find('A.links[data-stdt]').on('click', function(e) {
                         dgemHist.regStdtInfoDetlPopup($(e.target).data('stdt-no'), $(e.target).data('guar-no'))
